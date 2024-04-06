1da2aae41fb4730472dfa8252193e6e5fa1d41f7
Sacbank <110980102+Sacbank@users.noreply.github.com>
2024-04-06 10:38:11 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mytodoappbucket"
+    bucket = "test-blaze-bucket"
     key    = "eks/terraform.tfstate"
-    region = "us-east-2"
+    region = "us-east-1"
   }
 }
